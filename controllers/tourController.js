56a1d5b0628c09d716fb8e35463860cdad33f7bd
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-03 21:03:24 +0700

    #101. Tái cấu trúc API Features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 const createTour = async (req, res) => {
     try {
@@ -31,61 +32,16 @@ const aliasTopTours = (req, res, next) => {
 
 const getAllTours = async (req, res) => {
     try {
-        //-------BUILD QUERY
-        //1A) Filtering
-        const queryObj = { ...req.query };
+        // EXECUTE QUERY
+        //---- Tour.find() return về một query
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        const tours = await features.query;
 
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach((el) => delete queryObj[el]);
-
-        //1B) Advance filtering: gte: greather than equal, gt: grather than,lte: less than equal
-
-        const regixAdvanceFilter = /\b(gte|gt|lte|lt)\b/g;
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(regixAdvanceFilter, (match) => `$${match}`);
-
-        let query = Tour.find(JSON.parse(queryStr));
-
-        //2)SORTING
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            //Có thể sử dụng xâu chuỗi nhiều phương thức query
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort('-createdAt');
-        }
-
-        //3) LIMITTING
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-        } else {
-            //Select tất cả ngoại trừ field: "__v"
-            query = query.select('-__v');
-        }
-
-        //4)Pagination
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments();
-            if (skip >= numTours) throw new Error('This page dose not Exist');
-        }
-
-        //-------EXECUTE QUERY
-        const tours = await query;
-
-        // const query =  Tour.find()
-        //     .where('duration')
-        //     .equals(5)
-        //     .where('difficulty')
-        //     .equals('easy');
-
-        //------- SEND RESPONSE
+        // SEND RESPONSE
         res.status(200).json({
             status: 'success',
             results: tours.length,
