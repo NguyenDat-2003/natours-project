b473e6aecf2a78042006c82f2fd8ae194112d811
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-06 10:05:33 +0700

    #115. Better Errors and Refactoring

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -25,27 +27,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+//4) Phần mềm trung gian xử lý lỗi (MiddleWare Error)
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Cant find  ${req.originalUrl} on this server!`,
-    // });
-    // next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-    err.statusCode = 404;
-    err.status = 'fail';
-    next(err);
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-//---Phần mềm trung gian xử lý lỗi
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
