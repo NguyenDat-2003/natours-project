2a112d9dd2907b2910c452673e9b3a4b3f9837e1
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-06 09:31:53 +0700

    #114. Implementing a Global Error Handling Middleware

--- app.js
+++ app.js
@@ -15,12 +15,7 @@ app.use(express.json());
 
 //---static file
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-    console.log('Hello from the middleware 👋');
-    next();
-});
-
+//----
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
@@ -31,11 +26,26 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Cant find  ${req.originalUrl} on this server!`,
-    });
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Cant find  ${req.originalUrl} on this server!`,
+    // });
     // next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.statusCode = 404;
+    err.status = 'fail';
+    next(err);
+});
+
+//---Phần mềm trung gian xử lý lỗi
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+    });
 });
 
 module.exports = app;
