df876b1e0689b9e88bb1b7c6f78677081fbf353c
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-08 22:17:04 +0700

    #129. Create Token, Signing up Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,18 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 const signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create(req.body);
 
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        //---JWT hết hạn sau 90 ngày
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
     res.status(201).json({
         status: 'success',
+        token,
         data: { user: newUser },
     });
 });
