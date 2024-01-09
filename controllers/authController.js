3e3042d1b368e6baf85e03331d47c28700b96ad7
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-09 20:26:22 +0700

    #134. Authorization: User Roles and Permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -96,4 +96,20 @@ const protect = catchAsync(async (req, res, next) => {
     req.user = currentUser;
     next();
 });
-module.exports = { signup, login, protect };
+
+const restrictTo = (...roles) => {
+    return (req, res, next) => {
+        // roles ['admin', 'lead-guide']. role='user'
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new AppError(
+                    'You do not have permission to perform this action',
+                    403
+                )
+            );
+        }
+
+        next();
+    };
+};
+module.exports = { signup, login, protect, restrictTo };
