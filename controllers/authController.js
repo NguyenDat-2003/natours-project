307119f0fab878202e20cae13f1454697b28ef79
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-09 11:08:58 +0700

    #130. Logging in Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,19 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
+const AppError = require('../utils/appError');
+
+const signToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        //---JWT hết hạn sau 90 ngày
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
 
 const signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create(req.body);
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-        //---JWT hết hạn sau 90 ngày
-        expiresIn: process.env.JWT_EXPIRES_IN,
-    });
+    const token = signToken(newUser._id);
 
     res.status(201).json({
         status: 'success',
@@ -17,4 +22,29 @@ const signup = catchAsync(async (req, res, next) => {
     });
 });
 
-module.exports = { signup };
+const login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // 1) Check if email and password exist
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password!', 400));
+    }
+
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
+
+    //----Tạo Methods correctPasswordd để mã hóa pass nhập vào và so sánh với pass trong db
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or password', 401));
+    }
+
+    // 3) If everything ok, send token to client
+    const token = signToken(user._id);
+
+    res.status(200).json({
+        status: 'success',
+        token,
+    });
+});
+
+module.exports = { signup, login };
