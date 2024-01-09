d740381d327856468ff558e9d599f6de490e4b9c
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-09 12:03:22 +0700

    #131. Authorization Protecting Tour Routes - Part 1

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,4 +47,25 @@ const login = catchAsync(async (req, res, next) => {
     });
 });
 
-module.exports = { signup, login };
+const protect = catchAsync(async (req, res, next) => {
+    // 1) Getting token and check of it's there
+    let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+        return next(
+            new AppError(
+                'You are not logged in! Please log in to get access.',
+                401
+            )
+        );
+    }
+
+    next();
+});
+module.exports = { signup, login, protect };
