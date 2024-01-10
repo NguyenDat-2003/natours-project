d94946eec051e62b4b71d5bf4f401aa40bb416b5
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-10 12:34:23 +0700

    #138. Updating the Current User: Password

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,16 +14,22 @@ const signToken = (id) => {
     });
 };
 
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user,
+        },
+    });
+};
+
 const signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create(req.body);
 
-    const token = signToken(newUser._id);
-
-    res.status(201).json({
-        status: 'success',
-        token,
-        data: { user: newUser },
-    });
+    createSendToken(newUser, 201, res);
 });
 
 const login = catchAsync(async (req, res, next) => {
@@ -43,12 +49,7 @@ const login = catchAsync(async (req, res, next) => {
     }
 
     // 3) If everything ok, send token to client
-    const token = signToken(user._id);
-
-    res.status(200).json({
-        status: 'success',
-        token,
-    });
+    createSendToken(user, 200, res);
 });
 
 const protect = catchAsync(async (req, res, next) => {
@@ -183,14 +184,31 @@ const resetPassword = catchAsync(async (req, res, next) => {
 
     //----------- 3) Update changedPasswordAt property for the user
     //----------- 4) Log the user in, send JWT
-    const token = signToken(user._id);
-
-    res.status(200).json({
-        status: 'success',
-        token,
-    });
+    createSendToken(user, 200, res);
 });
 
+const updatePassword = catchAsync(async (req, res, next) => {
+    // 1) Get user from collection
+    const user = await User.findById(req.user.id).select('+password');
+
+    // 2) Check if POSTed current password is correct
+    if (
+        !(await user.correctPassword(req.body.passwordCurrent, user.password))
+    ) {
+        return next(new AppError('Your current password is wrong.', 401));
+    }
+
+    // 3) If so, update password
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    await user.save();
+
+    //-- không sử dụng findByIdAndUpdate cho bất kì điều gì liên quan đếnm mật khẩu
+    // User.findByIdAndUpdate will NOT work as intended!
+
+    // 4) Log user in, send JWT
+    createSendToken(user, 200, res);
+});
 module.exports = {
     signup,
     login,
@@ -198,4 +216,5 @@ module.exports = {
     restrictTo,
     forgotPassword,
     resetPassword,
+    updatePassword,
 };
