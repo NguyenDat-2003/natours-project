4f3baf86fbc13e0aee2340bed98bc519468c93d2
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-10 20:29:32 +0700

    #143. Implementing Request Limiting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -13,8 +14,19 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-app.use(express.json());
 
+const limiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 60 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 60 minutes)
+    standardHeaders: 'draft-7', // draft-6: RateLimit-* headers; draft-7: combined RateLimit header
+    legacyHeaders: false, // X-RateLimit-* headers
+    message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+// Apply the rate limiting middleware to all requests
+app.use('/api', limiter);
+
+app.use(express.json());
 //---static file
 app.use(express.static(path.join(__dirname, 'public')));
 //----
