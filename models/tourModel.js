cac670016a3e66f1fcfb4db8838576d470c630a9
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-11 16:20:49 +0700

    #157. Virtual Populate: Tours and Reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -136,6 +136,14 @@ tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
 });
 
+// Virtual populate
+// Tạo ra một trường reviews nhưng không lưu vào db, để giữ một tham chiếu đến tất cả các tài liệu con (Review)
+tourSchema.virtual('reviews', {
+    ref: 'Review',
+    foreignField: 'tour',
+    localField: '_id',
+});
+
 // ----------------------------------DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
     // console.log(this);
