8b873cc5990de464453ef98bd9a1b5b286cdd4a1
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-16 20:22:08 +0700

    #193. Rendering Error Pages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,24 +1,35 @@
 const AppError = require('../utils/appError');
 
-const sendErrorDev = (err, res) => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack,
+const sendErrorDev = (err, req, res) => {
+    // A) API
+    if (req.originalUrl.startsWith('/api')) {
+        return res.status(err.statusCode).json({
+            status: err.status,
+            error: err,
+            message: err.message,
+            stack: err.stack,
+        });
+    }
+
+    // B) RENDERED WEBSITE
+    console.error('ERROR 💥', err);
+    return res.status(err.statusCode).render('error', {
+        title: 'Something went wrong!',
+        msg: err.message,
     });
 };
 
-const sendErrorProd = (err, res) => {
-    // Operational, trusted error: send message to client
-    if (err.isOperational) {
-        res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message,
-        });
-
-        // Programming or other unknown error: don't leak error details
-    } else {
+const sendErrorProd = (err, req, res) => {
+    // A) API
+    if (req.originalUrl.startsWith('/api')) {
+        // A) Operational, trusted error: send message to client
+        if (err.isOperational) {
+            res.status(err.statusCode).json({
+                status: err.status,
+                message: err.message,
+            });
+        }
+        // B) Programming or other unknown error: don't leak error details
         // 1) Log error
         console.error('ERROR 💥', err);
 
@@ -28,6 +39,23 @@ const sendErrorProd = (err, res) => {
             message: 'Something went very wrong!',
         });
     }
+
+    // B) RENDERED WEBSITE
+    // A) Operational, trusted error: send message to client
+    if (err.isOperational) {
+        return res.status(err.statusCode).render('error', {
+            title: 'Something went wrong!',
+            msg: err.message,
+        });
+    }
+    // B) Programming or other unknown error: don't leak error details
+    // 1) Log error
+    console.error('ERROR 💥', err);
+    // 2) Send generic message
+    return res.status(err.statusCode).render('error', {
+        title: 'Something went wrong!',
+        msg: 'Please try again later.',
+    });
 };
 
 const handleCastErrorDB = (err) => {
@@ -64,7 +92,7 @@ module.exports = (err, req, res, next) => {
     err.status = err.status || 'error';
 
     if (process.env.NODE_ENV === 'development') {
-        sendErrorDev(err, res);
+        sendErrorDev(err, req, res);
     } else if (process.env.NODE_ENV === 'production') {
         let error = { ...err };
 
@@ -75,6 +103,6 @@ module.exports = (err, req, res, next) => {
         if (err.name === 'JsonWebTokenError') error = handleJWTError();
         if (err.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
-        sendErrorProd(error, res);
+        sendErrorProd(error, req, res);
     }
 };
