4b43b406974dd04e0ba0e862c29083da690d3903
NguyenDat-2003 <maxdat2003@gmail.com>
2024-01-19 10:39:01 +0700

    #211. Integrating Stripe into the Back-End

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 
 const app = express();
 
@@ -57,6 +58,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 //4) Phần mềm trung gian xử lý lỗi (MiddleWare Error)
 app.all('*', (req, res, next) => {
